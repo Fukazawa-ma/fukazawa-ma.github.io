77204b42638095e3ba3e2627dccfcfcf2429268c
Fukazawa-ma <151440839+Fukazawa-ma@users.noreply.github.com>
2024-05-15 13:42:45 +0900

    add loading animation

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ $('.hamburger').click(function(){
     $(this).toggleClass("active");
     $('#g-nav').toggleClass("navactive");
 })
+
+$(window).on('load',function(){
+    $("#loader").delay(3500).fadeOut('slow');
+    $("#loading").delay(3500).fadeOut('slow');
+  });
